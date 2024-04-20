62e5f2d300e9a503a4b60b422a76eef1952be1bf
malexj <alexmaximovj@gmail.com>
2024-04-20 09:55:29 +0300

    Add exclude test impl

--- src/main/java/com/malex/service/filter/SubscriptionCriteriaFilteringService.java
+++ src/main/java/com/malex/service/filter/SubscriptionCriteriaFilteringService.java
@@ -1,20 +1,20 @@
 package com.malex.service.filter;
 
+import static com.malex.model.filter.ConditionType.EXCLUDE;
 import static com.malex.model.filter.ConditionType.INCLUDE;
 
 import com.malex.model.entity.FilterEntity;
 import com.malex.model.filter.ConditionType;
 import com.malex.model.filter.FilterCondition;
 import com.malex.service.storage.FilterStorageService;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /** Subscription criteria (include or exclude keywords) filtering service */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SubscriptionCriteriaFilteringService {
@@ -23,7 +23,6 @@ public class SubscriptionCriteriaFilteringService {
 
   /** Apply filtering of rss topics by criteria */
   public boolean applyFilterByCriteria(String text, List<String> topicFilterIds) {
-
     // 1. find all topic filter ids
     if (Objects.isNull(text) || topicFilterIds.isEmpty()) {
       return true;
@@ -45,9 +44,22 @@ public class SubscriptionCriteriaFilteringService {
 
     // 4. find include matching
     boolean includeAnyMatch =
-        filterConditions.get(INCLUDE).stream().anyMatch(key -> findOccurrencePhrase(text, key));
+        filterConditions.entrySet().stream()
+            .filter(entry -> INCLUDE == entry.getKey())
+            .map(Map.Entry::getValue)
+            .flatMap(Collection::stream)
+            .anyMatch(key -> findOccurrencePhrase(text, key));
 
     // 5. find exclude matching
+    boolean excludeNoneMatch =
+        filterConditions.entrySet().stream()
+            .filter(entry -> EXCLUDE == entry.getKey())
+            .map(Map.Entry::getValue)
+            .flatMap(Collection::stream)
+            .noneMatch(key -> findOccurrencePhrase(text, key));
+
+    log.info(
+        "Filter: include anyMatch - {}, exclude noneMatch - {}", includeAnyMatch, excludeNoneMatch);
 
     return includeAnyMatch;
   }
