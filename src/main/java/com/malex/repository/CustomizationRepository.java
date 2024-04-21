9b8bb368985cc0d017a6bc5ed5793bcd3a278c4d
malexj <alexmaximovj@gmail.com>
2024-04-21 22:44:15 +0300

    Add customization

--- src/main/java/com/malex/repository/CustomizationRepository.java
+++ src/main/java/com/malex/repository/CustomizationRepository.java
@@ -1,8 +1,12 @@
 package com.malex.repository;
 
 import com.malex.model.entity.CustomizationEntity;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomizationRepository extends MongoRepository<CustomizationEntity, String> {}
+public interface CustomizationRepository extends MongoRepository<CustomizationEntity, String> {
+
+  Optional<CustomizationEntity> findCustomizationEntityById(String id);
+}
--- src/main/java/com/malex/service/storage/CustomizationStorageService.java
+++ src/main/java/com/malex/service/storage/CustomizationStorageService.java
@@ -5,6 +5,7 @@ import com.malex.model.dto.CustomizationDto;
 import com.malex.model.request.CustomizationRequest;
 import com.malex.repository.CustomizationRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,10 @@ public class CustomizationStorageService {
 
   public List<CustomizationDto> findAll() {
     return repository.findAll().stream().map(mapper::entityToDto).toList();
+  }
+
+  public Optional<CustomizationDto> findById(String id) {
+    return repository.findById(id).map(mapper::entityToDto);
   }
 
   public CustomizationDto save(CustomizationRequest request) {
