34e5b463b75cba539e4aa72cc3abdcad0434b005
malexj <alexmaximovj@gmail.com>
2025-03-05 09:43:22 +0200

    Fix filter

--- src/main/java/com/malex/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/malex/configuration/WebSecurityConfiguration.java
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FilterConfiguration {
+public class WebSecurityConfiguration {
+
+  /*
+   * REST api
+   */
+  private static final String REST_API_URL_PATTERN = "/v1/*";
 
   @Bean
-  public FilterRegistrationBean<JwtTokenFilter> jwtFilter(JwtTokenFilter jwtTokenFilter) {
+  public FilterRegistrationBean<JwtTokenFilter> jwtFilter() {
     FilterRegistrationBean<JwtTokenFilter> registrationBean = new FilterRegistrationBean<>();
-    registrationBean.setFilter(jwtTokenFilter);
-    registrationBean.addUrlPatterns("/v1/*");
+    registrationBean.setFilter(new JwtTokenFilter());
+    registrationBean.addUrlPatterns(REST_API_URL_PATTERN);
     return registrationBean;
   }
 }
